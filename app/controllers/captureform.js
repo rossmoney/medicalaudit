15c41f70f00c3530f467c6efe226bdb3d3b6e9f6
Ross Money <Ross Money>
2016-09-08 11:18:58 +0100

    more fixes

--- app/controllers/captureform.js
+++ app/controllers/captureform.js
@@ -1,5 +1,40 @@
 'use strict';
 
+function dateDifference(start, end) {
+
+  // Copy date objects so don't modify originals
+  var s = new Date(+start);
+  var e = new Date(+end);
+
+  // Set time to midday to avoid dalight saving and browser quirks
+  s.setHours(12,0,0,0);
+  e.setHours(12,0,0,0);
+
+  // Get the difference in whole days
+  var totalDays = Math.round((e - s) / 8.64e7);
+
+  // Get the difference in whole weeks
+  var wholeWeeks = totalDays / 7 | 0;
+
+  // Estimate business days as number of whole weeks * 5
+  var days = wholeWeeks * 5;
+
+  // If not even number of weeks, calc remaining weekend days
+  if (totalDays % 7) {
+    s.setDate(s.getDate() + wholeWeeks * 7);
+
+    while (s < e) {
+      s.setDate(s.getDate() + 1);
+
+      // If day isn't a Sunday or Saturday, add to business days
+      if (s.getDay() != 0 && s.getDay() != 6) {
+        ++days;
+      }
+    }
+  }
+  return days;
+};
+
 angular.module('myApp.captureform', ['ngRoute', 'ngResource'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -38,7 +73,16 @@ $scope.checkDates = function() {
 	if($scope.patient.numdaystaken > 0 && !isNaN($scope.patient.scanundertaken)) {
 		delete $scope.patient.refrecieved;
 		alert('Referral recieved date must be before the scan undertaken date.');
+		return;
 	}
+	var d1 = $scope.patient.refrecieved;
+	  var d2 = $scope.patient.scanundertaken;
+	  $scope.patient.numdaystaken = dateDifference(new Date(d1), new Date(d2));
+	  if($scope.patient.numdaystaken <= 3) {
+		$('#reasonfordelay_container').hide();
+	  } else {
+		$('#reasonfordelay_container').show();
+	  }
 };
 
 $scope.addPatient = function() {
