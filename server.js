559d37a35f939455c2daec2dc94c1955b1d4e354
Ross Money <Ross Money>
2016-09-10 15:04:27 +0100

    server

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+cors = require('cors'),
+bodyParser = require("body-parser"),
+mongoose = require('mongoose'),
+fs = require('fs');
+
+var mongoUri = 'mongodb://medicalaudit:medicalaudit@ds147995.mlab.com:47995/medicalaudit';
+//var mongoUri = 'mongodb://localhost/patients';
+mongoose.connect(mongoUri);
+var db = mongoose.connection;
+db.on('error', function () {
+  throw new Error('unable to connect to database at ' + mongoUri);
+});
+
+var app = express();
+app.use(express.static(__dirname + '/app'));
+app.use(bodyParser.json());
+app.use(cors());
+
+require('./app/models/patient');
+require('./app/models/user');
+require('./app/models/session');
+require('./app/routes')(app);
+
+app.listen(process.env.PORT || 8000);
+console.log('Listening on port 8000...');
